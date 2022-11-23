a50c8569e3804c201debe1383df4ca0c2492676c
root <root@localhost.localdomain>
2022-11-23 18:53:05 -0500

    yum install

--- static.sh
+++ static.sh
@@ -1,4 +1,6 @@
-cd /data/pmt/
+cd /data/tools/pmt/
+#yum install glibc-static libstdc++-static
+# -std=c++11
 g++  main.cpp -o pmt  \
   -std=c++11 -Ipcm/src/ -Lbuild/lib/ -lpcm -lpthread -ldl libpcm.a -static
 #./pmt_static 2>/dev/null
