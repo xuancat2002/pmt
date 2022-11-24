0b4945a10bf92662b3072eab47ec399a18621032
WSUN <weixing.sun@gmail.com>
2022-11-23 19:12:26 -0500

    lib

--- static.sh
+++ static.sh
@@ -1,6 +1,5 @@
 cd /data/tools/pmt/
 #yum install glibc-static libstdc++-static
-# -std=c++11
 g++  main.cpp -o pmt  \
-  -std=c++11 -Ipcm/src/ -Lbuild/lib/ -lpcm -lpthread -ldl libpcm.a -static
+  -std=c++11 -Ipcm/src/ -Llib/ -lpcm -lpthread -ldl -static  # libpcm.a
 #./pmt_static 2>/dev/null
