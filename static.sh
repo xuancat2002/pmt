50f1a14fa64765ecbc725b09f6ecc570946c66bc
WSUN <weixing.sun@gmail.com>
2022-12-06 21:10:54 -0500

    compile -> test

--- static.sh
+++ static.sh
@@ -1,4 +1,9 @@
 #cd /data/tools/pmt/
 #yum install glibc-static libstdc++-static
 #g++  main.cpp -o pmt -std=c++11 -Ipcm/src/ -Llib/ -lpcm -lpthread -ldl -static  # libpcm.a
+rm -rf pcie
 g++  pcie.cpp -o pcie -std=c++11 -Ipcm/src/ -Llib/ -lpcm -lpthread -ldl -static 
+
+
+ids=`lspci|grep acc|awk '{print $1}'| tr '\n' ','`
+./pcie --only=$ids
