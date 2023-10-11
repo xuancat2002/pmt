fcf2d0bfdca64ea2106eae3f249942717e70f740
Weixing Sun <weixingsun@users.noreply.github.com>
2023-10-11 20:09:36 +0900

    compile

--- static.sh
+++ static.sh
@@ -1,9 +1,9 @@
 #cd /data/tools/pmt/
 #yum install glibc-static libstdc++-static
-#g++  main.cpp -o pmt -std=c++11 -Ipcm/src/ -Llib/ -lpcm -lpthread -ldl -static  # libpcm.a
-rm -rf pcie
+rm -rf pcie mem 
+g++  main.cpp -o mem  -std=c++11 -Ipcm/src/ -Llib/ -lpcm -lpthread -ldl -static  # libpcm.a
 g++  pcie.cpp -o pcie -std=c++11 -Ipcm/src/ -Llib/ -lpcm -lpthread -ldl -static 
 
 
-ids=`lspci|grep acc|awk '{print $1}'| tr '\n' ','`
-./pcie --only=$ids
+#ids=`lspci|grep acc|awk '{print $1}'| tr '\n' ','`
+#./pcie --only=$ids
